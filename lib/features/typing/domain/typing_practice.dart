f5b61e4efb2fc6a062c50d0a07a54a2217dd49d3
BaldGhost-git <rizkyadityanugroho@gmail.com>
2026-01-01 20:57:55 +0700

    feat: Modify typing_practice.dart to include statistics tracking and test management

--- lib/features/typing/domain/typing_practice.dart
+++ lib/features/typing/domain/typing_practice.dart
@@ -1,5 +1,4 @@
 import 'package:freezed_annotation/freezed_annotation.dart';
-import 'package:typingapp/features/typing/domain/text.dart';
 import 'package:typingapp/features/typing/domain/typing_statistics.dart';
 
 part 'typing_practice.freezed.dart';
