0654ff84cc73cbf3978567237e33a23cd713354d
BaldGhost-git <rizkyadityanugroho@gmail.com>
2026-01-25 21:27:31 +0700

    feat: update sizes

--- lib/core/themes/sizes.dart
+++ lib/core/themes/sizes.dart
@@ -8,7 +8,7 @@ class AppSizes {
   static const double testCounterYAxis = -0.2;
   static const double testWidgetHeight = 200;
   static const double padBetweenWidgets = 15;
-  static const double chartWidgetHeight = 300;
+  static const double chartWidgetHeight = 350;
   static const double typingTextViewportHeight = testWidgetHeight * 0.6;
 
   // Sizes for typing text caret
@@ -21,7 +21,7 @@ class AppSizes {
 
   // Sizes for fonts
   static const double heading2 = 30.0;
-  static const double heading1 = 50.0;
+  static const double heading1 = 40.0;
   static const double menusFontSize = 16.0;
   static const double chartTitleTextSize = 14.0;
   static const double chartNumberTextSize = 12.0;
@@ -33,4 +33,5 @@ class AppSizes {
   static const List<int> primaryDashLine = [10, 10];
   static const double chartWpmInterval = 20.0;
   static const double chartTitleBoxSize = 40.0;
+  static const double chartTitleReservedArea = 20.0;
 }
